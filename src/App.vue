67dd4c5308fa281a3a969d5cc279cce347d3fdd9
Tiarnach O Riada <tiarnachreidy@gmail.com>
2021-06-08 13:07:42 +0100

    rm old navbar

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     </router-link>
     <router-link class="navbar-item" to="/">Home</router-link>
     <router-link class="navbar-item" to="/about">About</router-link>
-    <router-link class="navbar-item" to="/articles">Articles</router-link>
+    <router-link class="navbar-item" to="/articles">Sample Articles</router-link>
 <!--
     <div class="navbar-burger" data-target="navbar">
       <span></span>
@@ -25,43 +25,4 @@
   </div>
 </nav>
 <router-view/>
-<!--
-<section class="hero is-fullheight">
-  <div class="hero-head">
-    <nav class="navbar">
-      <div class="container">
-        <div class="navbar-brand">
-          <a class="navbar-item">
-            <img src="favicon.ico" alt="Logo"/>
-            <span> EOFIS</span>
-          </a>
-          <span class="navbar-burger" data-target="navbarMenuHeroB">
-            <span></span>
-            <span></span>
-            <span></span>
-          </span>
-        </div>
-        <div id="navbarMenuHeroB" class="navbar-menu">
-          <div class="navbar-end">
-            <router-link class="navbar-item" to="/">Home</router-link>
-            <router-link class="navbar-item" to="/about">About</router-link>
-            <router-link class="navbar-item" to="/articles">Articles</router-link>
-          </div>
-        </div>
-      </div>
-    </nav>
-  </div>
-
-    <router-view/>
-</section>
--->
-      <!-- <p class="title">         Title      </p>      <p class="subtitle">        Subtitle      </p> -->
-<!--
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/articles">Articles</router-link>
-  </div>
-  <router-view/>
--->
 </template>
