827db072b09adc3c5cef8c822f93006d46e144fc
Tiarnach O Riada <tiarnachreidy@gmail.com>
2021-06-08 18:56:53 +0100

    Diff qs

--- src/components/ArticleViewer.vue
+++ src/components/ArticleViewer.vue
@@ -1,24 +1,25 @@
 <template>
-<div>
-    <section class="section" v-for="article in FileThing" :key="article.id">
-        <h1 class="title">{{article.Title}}</h1>
-        <h2 class="subtitle">Flashcards automatically generated from <a :href="article.URL" target="_blank">this article.</a></h2>
-<div class="columns is-multiline">
-            <div v-for="card in article.Cards" :key="card.id" class="box column is-one-quarter">
+    <div>
+        <section class="section" v-for="article in FileThing" :key="article.id">
+            <h1 class="title">{{article.Title}}</h1>
+            <h2 class="subtitle">Flashcards automatically generated from <a :href="article.URL" target="_blank">this article.</a></h2>
+            <div class="columns is-multiline">
+                <div v-for="card in article.Cards" :key="card.id" class="box column is-one-quarter">
                     <p class="has-text-primary-dark">
                         {{card.Question}}
                     </p>
-                <p class="has-text-white-bis has-background-dark">
-                        {{card.Answer}}
-                </p>
+                    <p class="has-text-white-bis has-background-dark">
+                        <strong>{{card.Answer}}</strong>
+                    </p>
+                </div>
             </div>
-</div>
-    </section>
-</div>
+        </section>
+    </div>
 </template>
 
 <script>
-import FileThing from '../../public/p1_out-tr_cq.json';
+import FileThing from '../../public/p1_out-vht5_b.json'
+//import FileThing from '../../public/p1_out-tr_cq.json';
 export default {
   name: 'Articles',
   props: {
